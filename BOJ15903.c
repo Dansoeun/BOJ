38c465c32d5fdc6387303efcd5f9d56cc36123f1
Dansoeun <rlaskgus0801@gmail.com>
2025-01-31 15:01:18 +0900

    15903 정답본

--- BOJ15903.c
+++ BOJ15903.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-#define MAX_SIZE 1001
+#define MAX_SIZE 1005
 
 typedef struct 
 {
-    int heap[MAX_SIZE];
+    long long heap[MAX_SIZE];
     int size;
 }Heap;
 
-void min_heap_insert(Heap *h, int item)
+void min_heap_insert(Heap *h, long long item)
 {
     h->size+=1;
     int i=h->size; //마지막 위치에서 +1
@@ -22,10 +22,10 @@ void min_heap_insert(Heap *h, int item)
     h->heap[i]=item; //새로운 노드 삽입 
 }
 
-int min_heap_delete(Heap *h)
+long long min_heap_delete(Heap *h)
 {
-    int result=h->heap[1];
-    int temp=h->heap[h->size];
+    long long result=h->heap[1];
+    long long temp=h->heap[h->size];
     h->size-=1;
 
     int parent=1;
@@ -58,15 +58,26 @@ int min_heap_delete(Heap *h)
 
 void Solution(Heap *h, int m, int n)
 {
-    int x=0; int y=0;
-    int hap=0;
-    int answer=0;
+    long long x=0; long long y=0;
+    long long hap=0;
+    long long answer=0;
+    int j=1;
+    long long temp=0;
+
+    while ((temp=min_heap_delete(h))==0)
+    {
+        continue;
+    }
+
+    if (temp!=0)
+        min_heap_insert(h,temp);
 
     for(int i=0; i<m; i++)
     {
         x=min_heap_delete(h);
         y=min_heap_delete(h);
         hap=x+y;
+        //printf("x:%lld y:%lld hap:%lld\n",x,y,hap);
 
         min_heap_insert(h,hap);
         min_heap_insert(h,hap);
@@ -75,25 +86,29 @@ void Solution(Heap *h, int m, int n)
     while (h->size>0)
     {
         answer+=min_heap_delete(h);
+        //printf("h size:%d answer:%lld\n",h->size,answer);
     }
 
-    printf("%d\n",answer);
+    printf("%lld\n",answer);
 }
 
 int main(void)
 {
     int m=0; int n=0; char ch=' ';
-    int a=0;
+    long long a=0;
     Heap h;
+    //h.size=0;
 
     scanf("%d %d%c", &n, &m, &ch);
 
     for(int i=0; i<n; i++)
     {
-        scanf("%d", &a);
+        scanf("%lld", &a);
         min_heap_insert(&h,a);
     }
     getchar();
+
+
 
     Solution(&h,m,n);
     return 0;
